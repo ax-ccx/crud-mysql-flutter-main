45f928c49772225170553769c3dd636a7b8cab4c
Abdulalah Timem <ax-ccx@hotmail.com>
2023-02-07 00:46:01 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ktabna/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ktabna
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mr.fahad/flutterDev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Applications/XAMPP/xamppfiles/htdocs/crud-mysql-flutter-main"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Applications/XAMPP/xamppfiles/htdocs/crud-mysql-flutter-main/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Applications/XAMPP/xamppfiles/htdocs/crud-mysql-flutter-main/.dart_tool/package_config.json"
